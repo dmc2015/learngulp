0dd8d5e3243a15f6ff7a5876b0a2406b397740bb
Don McLamb <mclamb.donald@gmail.com>
2015-08-03 23:56:38 -0400

    added glup requirments to gulpfile.js

--- hmphry_learning-gulp/my_gulp_project/gulpfile.js
+++ hmphry_learning-gulp/my_gulp_project/gulpfile.js
@@ -1,3 +1,15 @@
-var gulp = require('gulp');
+//Requirements
+var gulp = require('gulp'),
+sass = requrie('gulp-ruby-sass');
+
+//Defines Path to SaSS
+var sassRoot = 'build/sass/';
+
+//Gulp Task
+gulp.task('sass-to-css', funciton(){
+	return gulp.src(sassRoot+'main.scss')
+	.pipe(sass({sourcemap: true}))
+	.pipe(gulp.dest('public/css'));
+});
 
 
