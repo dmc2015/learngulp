e92c3c9a92c5ea53dc4392a15d429302dd628477
Don McLamb <mclamb.donald@gmail.com>
2015-08-05 23:39:27 -0400

    added gulpfile for travismaynar project

---
+++ travismaynar-gulp/my-gulp-project/gulpfile.js
@@ -0,0 +1,13 @@
+//include gulp
+var gulp = require('gulp');
+
+//Include plugins
+//
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
