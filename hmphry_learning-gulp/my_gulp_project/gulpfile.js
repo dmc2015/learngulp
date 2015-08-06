02e6ef519adc3f03c84bad9b6deb395fe2875e20
Don McLamb <mclamb.donald@gmail.com>
2015-08-06 16:57:56 -0400

    got hmphry tut workign

--- hmphry_learning-gulp/my_gulp_project/gulpfile.js
+++ hmphry_learning-gulp/my_gulp_project/gulpfile.js
@@ -5,11 +5,21 @@ var sass = require('gulp-ruby-sass');
 //Defines Path to SaSS
 var sassRoot = 'build/sass/';
 
+
 //Gulp Task
 gulp.task('sass-to-css', function(){
-	return gulp.src(sassRoot+'main.scss')
-	.pipe(sass({sourcemap: true}))
+	return sass(sassRoot+'main.sass', {style: 'expanded'})
+	.pipe(sass({souremap: 'true'}))
 	.pipe(gulp.dest('public/css'));
+});
+
+
+
+
+gulp.task('styles', function() {
+return sass('build/scss', { style: 'expanded' })
+    // .pipe(gulp.dest('scss'))
+    .pipe(gulp.dest(''))
 });
 
 //
