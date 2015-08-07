929efdee1d985879043daee1134aa7ed63c02e2c
Don McLamb <mclamb.donald@gmail.com>
2015-08-07 14:20:26 -0400

    added to tutorial and finished scoth.io-gulp tut

--- scotch.io-gulp/gulpfile.js
+++ scotch.io-gulp/gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require("gulp"),
     jshint = require('gulp-jshint'),
-    sass = require('gulp-sass');
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    gutil = require('gulp-util');
 
 
 gulp.task('sass-2-ccs', function(){
 	return gulp.src('source/sass/**/*.sass')
 		.pipe(sass())
+    .pipe(sourcemaps())
+    .pipe(sourcemaps.write())
 		.pipe(gulp.dest('public/assets/stylesheets'));
 });
 
@@ -15,8 +19,18 @@ gulp.task('jshint', function(){
 		.pipe(jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('concatjs', function(){
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js')
+    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/javascript'));
+});
+
 gulp.task('watch', function(){
 	gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/sass/**/*.sass', ['sass-2-css']);
 });
 
 gulp.task('default', ['watch']);
